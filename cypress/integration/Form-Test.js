7ba32b3163500cd5f3bcd0958238a4ef85f0fb5b
salza323 <sal.zamora480@gmail.com>
2020-09-17 16:22:13 -0700

    type name in username test done

--- cypress/integration/Form-Test.js
+++ cypress/integration/Form-Test.js
@@ -5,7 +5,16 @@ describe('user onboarding app', () => {
         cy.visit('http://localhost:3000')
     })
 
+    const userName = () => cy.get('input[name=userName]')
+    const userEmail = () => cy.get('input[name=userEmail]')
+    const userPassword = () => cy.get('input[name=userPassword]')
+    const termsOfService = () => cy.get('input[name=termsOfService]')
+
     it('test of test', () => {
         expect(1+2).to.equal(3)
+    })
+
+    it('mvp test', () => {
+        userName().type('Sample Name')
     })
 })
